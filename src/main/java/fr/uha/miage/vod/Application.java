1ddb15049d8150a0c480905e84db3470bd3f168e
Rita-chan <tenma971@hotmail.fr>
2016-10-22 11:05:59 +0200

    Page index

--- src/main/java/fr/uha/miage/vod/Application.java
+++ src/main/java/fr/uha/miage/vod/Application.java
@@ -108,7 +108,6 @@ public class Application implements CommandLineRunner {
 		film2.setSynopsis("Synopsis passionnant du deuxième film");
 		film2.setDuree("30");
 		film2.setJaquette("ch2.jpg");
-		
 		film2.setSortie(2016);
 		film2.setVideo("Là");
 		film2.setVersion("VF");
@@ -116,6 +115,60 @@ public class Application implements CommandLineRunner {
 		film2.setActeurs(acte2);
 		film2.setRealisateur(real2);
 		film2.setPays(pays2);
+		
+		Film film3 = new Film();
+		film3.setTitre("Titre3");
+		film3.setSynopsis("Synopsis passionnant du troisième film");
+		film3.setDuree("50 min");
+		film3.setJaquette("ch3.jpg");
+		film3.setSortie(2015);
+		film3.setVideo("Là");
+		film3.setVersion("VO");
+		film3.setCategories(cate1);
+		film3.setActeurs(acte1);
+		film3.setRealisateur(real1);
+		film3.setPays(pays1);
+			
+		
+		Film film4 = new Film();
+		film4.setTitre("Titre4");
+		film4.setSynopsis("Synopsis passionnant du quatrième film");
+		film4.setDuree("30 min");
+		film4.setJaquette("ch4.jpg");
+		film4.setSortie(2016);
+		film4.setVideo("Là");
+		film4.setVersion("VF");
+		film4.setCategories(cate2);
+		film4.setActeurs(acte2);
+		film4.setRealisateur(real2);
+		film4.setPays(pays2);
+		
+		Film film5 = new Film();
+		film5.setTitre("Titre5");
+		film5.setSynopsis("Synopsis passionnant du cinquième film");
+		film5.setDuree("75 min");
+		film5.setJaquette("c1.jpg");
+		film5.setSortie(2015);
+		film5.setVideo("Là");
+		film5.setVersion("VO");
+		film5.setCategories(cate1);
+		film5.setActeurs(acte1);
+		film5.setRealisateur(real1);
+		film5.setPays(pays1);
+			
+		
+		Film film6 = new Film();
+		film6.setTitre("Titre6");
+		film6.setSynopsis("Synopsis passionnant du sixième film");
+		film6.setDuree("60 min");
+		film6.setJaquette("cc2.jpg");
+		film6.setSortie(2016);
+		film6.setVideo("Là");
+		film6.setVersion("VF");
+		film6.setCategories(cate1);
+		film6.setActeurs(acte2);
+		film6.setRealisateur(real2);
+		film6.setPays(pays2);
 		
 		
 		acteurRepository.save(act1);
@@ -128,24 +181,44 @@ public class Application implements CommandLineRunner {
 		realisateurRepository.save(real2);
 		filmRepository.save(film1);
 		filmRepository.save(film2);
+		filmRepository.save(film3);
+		filmRepository.save(film4);
+		filmRepository.save(film5);
+		filmRepository.save(film6);
 		
 		act1.ajouterFilm(film1);
 		act2.ajouterFilm(film2);
+		act1.ajouterFilm(film3);
+		act2.ajouterFilm(film4);
+		act1.ajouterFilm(film5);
+		act2.ajouterFilm(film6);
 		acteurRepository.save(act1);
 		acteurRepository.save(act2);
 		
 		cat1.ajouterFilm(film1);
 		cat2.ajouterFilm(film2);
+		cat1.ajouterFilm(film3);
+		cat2.ajouterFilm(film4);
+		cat1.ajouterFilm(film5);
+		cat1.ajouterFilm(film6);
 		categorieRepository.save(cat1);
 		categorieRepository.save(cat2);
 		
 		pays1.ajouterFilm(film1);
 		pays2.ajouterFilm(film2);
+		pays1.ajouterFilm(film3);
+		pays2.ajouterFilm(film4);
+		pays1.ajouterFilm(film5);
+		pays2.ajouterFilm(film6);
 		paysRepository.save(pays1);
 		paysRepository.save(pays2);
 		
 		real1.ajouterFilm(film1);
 		real2.ajouterFilm(film2);
+		real1.ajouterFilm(film3);
+		real2.ajouterFilm(film4);
+		real1.ajouterFilm(film5);
+		real2.ajouterFilm(film6);
 		realisateurRepository.save(real1);
 		realisateurRepository.save(real2);
 	
