c7225a85358b1313b04c8e718eb1afe8ca369333
aniita31 <aniita.jean@hotmail.com>
2016-10-23 12:16:31 +0200

    Gestion statistique 1

--- src/main/java/fr/uha/miage/vod/controller/WebController.java
+++ src/main/java/fr/uha/miage/vod/controller/WebController.java
@@ -1,5 +1,7 @@
 package fr.uha.miage.vod.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import fr.uha.miage.vod.model.Categorie;
 import fr.uha.miage.vod.model.Film;
+import fr.uha.miage.vod.model.Utilisateur;
 import fr.uha.miage.vod.repository.CategorieRepository;
 import fr.uha.miage.vod.repository.FilmRepository;
+import fr.uha.miage.vod.repository.UtilisateurRepository;
 
 @Controller
 public class WebController {
@@ -18,6 +22,9 @@ public class WebController {
 	
 	@Autowired
 	private FilmRepository filmRepository;
+	
+	@Autowired
+	private UtilisateurRepository utilisateurRepository;
 	
 	@GetMapping("/")
 	public String home(Model model){
@@ -30,7 +37,25 @@ public class WebController {
 	}
 	
 	@GetMapping("/adminindex")
-	public String adminindex(){
+	public String adminindex(Model model){
+		
+		/*
+		 * Gestion des statistiques
+		 */
+		
+		Iterable<Categorie> listeCategorie = categorieRepository.findAll();
+		int nbCategorie = ((List<Categorie>) listeCategorie).size();	
+		model.addAttribute("nbCategorie", nbCategorie);
+		model.addAttribute("categories", listeCategorie);
+		
+		Iterable<Utilisateur> listeUtilisateur = utilisateurRepository.findAll();
+		int nbUtilisateur = ((List<Utilisateur>) listeUtilisateur).size();	
+		model.addAttribute("nbUtilisateur", nbUtilisateur);
+		
+		Iterable<Film> listeFilm = filmRepository.findAll();
+		int nbFilm = ((List<Film>) listeFilm).size();	
+		model.addAttribute("nbFilm", nbFilm);
+		
 		return "adminindex";
 	}
 	
